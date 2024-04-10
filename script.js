950cddbc8171f032a2371459dcf2bec42f153fc5
Ash Marshall <ashley_marshall86@hotmail.co.uk>
2024-04-10 14:48:16 +0000

    change images to emojis for simpler code for now

--- script.js
+++ script.js
@@ -50,19 +50,8 @@ const generateGame = () => {
         throw new Error("The dimension of the board must be an even number.")
     }
 
-    const images = [
-    {'img': 'assets/images/banana.jpg'},
-    {'img':'assets/images/dragon.jpg'},
-    {'img': 'assets/images/monster.jpg'},
-    {'img': 'assets/images/nose.jpg'},
-    {'img': 'assets/images/piggin.jpg'},
-    {'img': 'assets/images/question.jpg'},
-    {'img': 'assets/images/skull.jpg'},
-    {'img':'assets/images/tongue.jpg'},
-    {'img': 'assets/images/trex.jpg'},
-    {'img': 'assets/images/unicorn.jpg'},
-    ]
-    const picks = pickRandom(images, (dimensions * dimensions) / 2) 
+    const emojis = ['🍻', '👍', '👾', '👣', '🐶', '😴', '🍕', '🍔', '⚽', '🛹']
+    const picks = pickRandom(emojis, (dimensions * dimensions) / 2) 
     const items = shuffle([...picks, ...picks])
     const cards = `
         <div class="board" style="grid-template-columns: repeat(${dimensions}, auto)">
